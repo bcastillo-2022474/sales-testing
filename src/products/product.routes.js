322d13b6cd88ff11094e93d800e8b809b65c6c31
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-03-10 07:49:08 -0600

    Products crud done, routes working

--- src/products/product.routes.js
+++ src/products/product.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Product from "./product.model.js";
 import Category from "../category/category.model.js";
-import { body, query } from "express-validator";
+import { body, param, query } from "express-validator";
 import { validateRequestParams } from "../middleware/validate-request-params.js";
 import { validateJwt } from "../middleware/validate-jwt.js";
 import { isAdminLogged } from "../middleware/is-logged.js";
@@ -25,7 +25,11 @@ router
 
       const [total, products] = await Promise.allSettled([
         Product.countDocuments(query),
-        Product.find(query).limit(limit).skip(offset),
+        Product.find(query)
+          .select("-tp_status -__v")
+          .populate("category", "name")
+          .limit(limit)
+          .skip(offset),
       ]);
 
       res.status(200).json({
@@ -42,6 +46,15 @@ router
         .isString()
         .notEmpty()
         .isLength({ min: 4 }),
+
+      body("name", "Name already in use").custom(async (name) => {
+        const product = await Product.findOne({ name });
+
+        if (!product) return true;
+
+        throw new Error("Name already in use");
+      }),
+
       body(
         "description",
         "Description must be defined and must have at least 10 characters and less than 200",
@@ -65,15 +78,140 @@ router
         return res.status(400).json({ message: "Category not found" });
       }
 
-      const product = new Product({
+      const product = await new Product({
         name,
         description,
-        category: category.name,
+        category: category._id,
       });
+      console.log(product);
+
       await product.save();
 
-      res.status(201).json(product);
+      res.status(201).json({
+        name,
+        description,
+        category: {
+          name: category.name,
+        },
+      });
     },
   );
+
+router
+  .route("/:id")
+  .get(
+    [
+      validateJwt,
+      isAdminLogged,
+      param("id", "Id Must be a MongoID").isMongoId(),
+      validateRequestParams,
+    ],
+    async (req, res) => {
+      const { id } = req.params;
+
+      const product = await Product.findOne({ _id: id, tp_status: true })
+        .select("name description category")
+        .populate("category", "name");
+
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+
+      res.status(200).json(product);
+    },
+  )
+  .put(
+    [
+      validateJwt,
+      isAdminLogged,
+      param("id", "Id Must be a MongoID").isMongoId(),
+      body("name", "If defined, name must have at least 4 characters")
+        .optional()
+        .isLength({ min: 4 }),
+
+      body("name", "Name already in use").custom(async (name, { req }) => {
+        const product = await Product.findOne({
+          name,
+          $nor: [{ _id: req.params.id }],
+        });
+
+        if (!product) return true;
+
+        throw new Error("Name already in use");
+      }),
+
+      body(
+        "description",
+        "If defined, description must have at least 10 characters and less than 200",
+      )
+        .optional()
+        .isLength({ max: 200, min: 10 }),
+      validateRequestParams,
+    ],
+    async (req, res) => {
+      const { id } = req.params;
+      const { name, description, category: categoryName } = req.body;
+
+      const category = await Category.findOne({
+        name: categoryName,
+        tp_status: true,
+      });
+
+      if (!category) {
+        return res.status(400).json({ message: "Category not found" });
+      }
+
+      const updatedProduct = { name, description, category: category._id };
+
+      Object.keys(updatedProduct).forEach((key) => {
+        if (!updatedProduct[key]) {
+          delete updatedProduct[key];
+        }
+      });
+
+      console.log({ updatedProduct, id });
+
+      const product = await Product.findOneAndUpdate(
+        {
+          _id: id,
+          tp_status: true,
+        },
+        updatedProduct,
+      );
+
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+
+      res.status(200).json({
+        name,
+        description,
+        category: {
+          name: category.name,
+        },
+      });
+    },
+  )
+  .delete([], async (req, res) => {
+    const { id } = req.params;
+
+    const product = await Product.findOneAndUpdate(
+      {
+        _id: id,
+        tp_status: true,
+      },
+      {
+        tp_status: false,
+      },
+    )
+      .select("-__v -tp_status")
+      .populate("category", "name");
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  });
 
 export default router;
